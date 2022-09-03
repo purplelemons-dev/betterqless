cad368d0438e9bdd3ffb18b07028e8d1250c0b83
BuildTools <unconfigured@null.spigotmc.org>
2022-09-03 13:49:55 -0500

    logging in and out!

--- server.py
+++ server.py
@@ -8,6 +8,9 @@ def fdir(name:str, extra:str=""):
     directory = '\\'.join(__file__.split('\\')[:-1])
     return f"{directory}\\{extra}{name}"
 
+def parseCookie(cookie:str):
+    return {item.split('=')[0]:item.split('=')[1] for item in cookie.split('; ')}
+
 class userSystem:
 
     def __init__(self):
@@ -98,10 +101,10 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         self.send_header('Location', url)
         self.end_headers()
 
-    def _200(self, filename: str=None, content: str=None):
+    def _200(self, filename: str=None, content: str=None, custom_headers:dict[str,str]={}, **replace:str):
         """
         Send a 200 response with the content of the file or otherwise specified content.
-        
+        Will replace any variables in the form of `{{var}}` with the value of the `var` key in the `replace` dictionary.
         """
         content_type = "text/html"
         if filename is None and content is None:
@@ -121,8 +124,12 @@ class Handler(http.server.SimpleHTTPRequestHandler):
                 elif filename.endswith(".png"):
                     content_type = "image/png"
 
+        for key, value in replace.items():
+            content = content.replace("{{"+key+"}}", value)
+
         self.send_response(200)
         self.send_header('Content-type', content_type)
+        for key, value in custom_headers.items(): self.send_header(key, value)
         self.end_headers()
         self.wfile.write(content.encode())
 
@@ -131,14 +138,38 @@ class Handler(http.server.SimpleHTTPRequestHandler):
             self.redirect("/login")
             return
 
-        elif self.path=="/favicon.ico":
-            print("favicon.ico")
-            self._200(filename="static\\favico.png")
-            return
-
-        elif self.path=="/login":
+        elif self.path.startswith("/login"):
+            # Check if the logout=true query is present
+            if "logout=true" in self.path:
+                # Delete the token cookie
+                self._200(filename="login.html", custom_headers={"Set-Cookie": "token=;SameSite=Strict;Expires=Thu, 01 Jan 1970 00:00:00 GMT;"})
+                return
             self._200(filename="login.html")
             return
+
+        elif self.path=="/dashboard":
+            # validate token via request cookie
+            cookie = self.headers.get("Cookie")
+            token = parseCookie(cookie)["token"]
+
+            if token not in usersys.tokens:
+                self.redirect("/login")
+                return
+            username = usersys.tokens[token]
+            for user in usersys.users:
+                if user["name"]==username:
+                    role = user["department"]
+                    break
+            else:
+                self._404("User not found")
+                return
+
+            if role=="admin":
+                self._200(filename="admindash.html", username=username)
+                return
+            elif role=="student_worker":
+                self._200(filename="workerdash.html", username=username)
+                return
 
         # If all other paths are not found, return something in the public directory
         self._200(filename=self.path[1:])
@@ -148,7 +179,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
             if self.headers["Content-Type"]!="application/json":
                 self._404("Content-Type must be application/json")
                 return
-            data:dict|list = json.loads(self.read_post())
+            data:dict = json.loads(self.read_post())
             token = usersys.authenticate(data["username"], data["password"])
             if token == -1:
                 self._401()
@@ -157,9 +188,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
                 self._404("User not found")
                 return
             # send the token as a cookie
-            self.send_response(200)
-            self.send_header("Set-Cookie", f"token={token};SameSite=Strict;")
-            self.end_headers()
+            self._200(content="", custom_headers={"Set-Cookie": f"token={token};SameSite=Strict;"})
             return
 
         self._404()
@@ -171,5 +200,3 @@ if __name__ == '__main__':
     with http.server.HTTPServer(('', 8000), Handler) as httpd:
         print("serving at port", 8000)
         httpd.serve_forever()
-
-
